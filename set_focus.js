21b74520f7b3dfd8f9b5015cff9185d34f9e0a9e
Bob Erb <bob.erb@gmail.com>
2012-03-03 23:45:05 -0500

    Initial version.

---
+++ set_focus.js
@@ -0,0 +1,22 @@
+// Focus on Craigslist Search is a Safari Extension that places
+// focus on the search box when a craigslist.org page is loaded.
+	
+// Copyright (C) 2011 Robert Erb
+
+// This file is part of Focus on Craigslist Search.
+
+// Focus on Craigslist Search is free software: you can redistribute it
+// and/or modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation, either version 3 of
+// the License, or (at your option) any later version.
+
+// Focus on Craigslist Search is distributed in the hope that it will
+// be useful, but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Focus on Craigslist Search.  If not, see
+// <http://www.gnu.org/licenses/>.  
+
+document.getElementById("query").focus();
